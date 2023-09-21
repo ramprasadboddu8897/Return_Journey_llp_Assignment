eb8af5d3687cb9c8a15e73a48c113f3233c54f2b
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-09-21 17:03:17 +0530

    Updated_Bug

--- config/twilio.js
+++ config/twilio.js
@@ -1,6 +1,6 @@
 // config/twilio.js
 export default {
   accountSid: 'AC41a42ac94701099081bc033f3004985a',
-  authToken: 'ec6032d3415621f4976b0425faf04377',
+  authToken: 'd02f6ae8d4df85ea23b0b463ee4e802f',
   phoneNumber: '+12566459446',
 };
